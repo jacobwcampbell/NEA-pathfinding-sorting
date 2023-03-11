8838dcc4ee3152ea1a0e2d2f6d2b88f9c0371f58
Jacob Campbell <jacobcampbell04@protonmail.com>
2023-03-11 17:45:12 +0000

    3

--- visualisation_suite/SortingSuite.py
+++ visualisation_suite/SortingSuite.py
@@ -1,2 +1,30 @@
 class SortingSuite():
-    pass
+    
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+    def setup(self) -> None:
+        pass
+
+    def main_loop(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
+    def handle_input(self) -> None:
+        pass
+
+    def load_config(self) -> dict:
+        pass
+
+    def validate_config(self, config) -> bool:
+        pass
+
+    def init_pygame(self) -> None:
+        pass
+
+
--- visualisation_suite/VisualisationSuite.py
+++ visualisation_suite/VisualisationSuite.py
@@ -13,18 +13,20 @@ class VisualisationSuite():
         choice = input("1. Sorting 2. Pathfinding \n: ") # Take user input
         isvalid, program = self.validate_choice(choice)
         if not isvalid:
-            print("Invalid Choice. Choices must be either the program number or name. \n")
-            self.choose_application()
-        self.program() # Call corresponding load function
+            print("Invalid Choice. Choices must be either the program number or name. \n") # Invalid choice handling
+            self.choose_application()                                                      
+        program() # Call corresponding load function                                  
 
-    def validate_choice(self,choice) -> tuple(bool, function):
-        if choice.isdigit():
+    def validate_choice(self,choice) :
+        if not isinstance(choice, str): # Rejects Non-String datatypes
+            return False, None
+        if choice.isdigit(): # Validate numerical choices
             if choice == "1":
                 return True, self.load_sorting
             elif choice == "2":
                 return True, self.load_pathfinding
             return False, None
-        if choice.upper() == "SORTING":
+        if choice.upper() == "SORTING": # Validate alphabetical choices
             return True, self.load_sorting
         elif choice.upper() == "PATHFINDING":
             return True, self.load_pathfinding
@@ -34,4 +36,18 @@ class VisualisationSuite():
         print("Pathfinding Suite has been successfully loaded!")    
     
     def load_sorting(self) -> None:
-        print("Sorting Suite has been successfully loaded!")
+        '''Loads the sorting suite.'''
+        app = SortingSuite()
+        app.run()
+
+
+if __name__ == '__main__':
+    v = VisualisationSuite()
+    print(v.validate_choice("1")) # Valid/Boundary
+    print(v.validate_choice("2")) # Valid/Boundary
+    print(v.validate_choice("0")) # Invalid
+    print(v.validate_choice(25)) # Erroneous
+    print(v.validate_choice("Pathfinding")) # Valid
+    print(v.validate_choice("PaTHfinDInG")) # Valid - Mixed Case
+    print(v.validate_choice("Hacking")) # Invalid
+
