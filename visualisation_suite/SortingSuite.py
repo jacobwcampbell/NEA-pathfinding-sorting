bfbabf0bb37e59d4e400be362da68fcdf63b72c7
Jacob Campbell <jacobcampbell04@protonmail.com>
2023-03-11 18:02:59 +0000

    4

--- visualisation_suite/SortingSuite.py
+++ visualisation_suite/SortingSuite.py
@@ -1,13 +1,38 @@
 class SortingSuite():
     
     def __init__(self) -> None:
-        pass
+        self.window = None
+        self.items = None
+        self.array_size = None
+        self.config_colours = None
+        self.dimensions = None
 
     def run(self) -> None:
-        pass
+        '''Sets up the application and then runs it.'''
+        self.setup()
+        self.main_loop()
 
     def setup(self) -> None:
-        pass
+        '''Calls the functions associated with loading configuration
+        values and creating the basic pygame structure'''
+        # load config values from external file
+        config = self.load_config() 
+        # assign config values to class attributes
+        self.array_size = config["array_size"] 
+        self.config_colours = config["config_colours"]
+        self.dimensions = config["dimensions"]
+        # initialize pygame
+        self.init_pygame()
+        # create pygame window
+        self.window = pygame.display.set_mode(config["dimensions"])
+        # create collection object
+        self.items = Collection(array_size=self.array_size,
+         config_colours=self.config_colours, dimensions=self.dimensions)
+        # generate bars to sort
+        self.items.generate_items()
+        # randomly shuffle bars to sort
+        self.items.shuffle_items()
+
 
     def main_loop(self) -> None:
         pass
